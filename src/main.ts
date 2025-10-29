e67bfe77719193e3e7f276fc7a3bbc9a6c925432
victrme <victor.azevedo@mail.com>
2025-10-29 10:27:19 +0100

    Update clock size during 10s

--- src/main.ts
+++ src/main.ts
@@ -17,12 +17,29 @@ domCentered.append(centeredClock.getContainer());
 textarea.value = JSON.stringify(charSizer.sizes, null, 2);
 
 basicClock.start();
+centeredClock.start(resizeOnMinutesAndTens);
 
-centeredClock.start(() => {
+/**
+ * Number "1" is really different than other numbers.
+ * This changes the clock size every minutes AND between
+ * 10 and 19 seconds
+ */
+function resizeOnMinutesAndTens() {
 	const text = domCentered.textContent ?? "";
-	const hoursMinutes = text.slice(0, text.length - 2);
-	const hoursMinutesSize = charSizer.sizeOf(hoursMinutes);
-	const secondsSize = numberAverage * 2;
+	const seconds = text.slice(6);
+	const isTens = seconds.startsWith("1");
+	let size = "";
 
-	domCentered.style.width = `${hoursMinutesSize + secondsSize}ch`;
-});
+	if (isTens) {
+		const hhmms = text.slice(0, 7);
+		const hhmmsSize = charSizer.sizeOf(hhmms);
+		size = `${hhmmsSize + numberAverage}`;
+	} else {
+		const hhmm = text.slice(0, 6);
+		const hhmmSize = charSizer.sizeOf(hhmm);
+		const ssSize = numberAverage * 2;
+		size = `${hhmmSize + ssSize}`;
+	}
+
+	domCentered.style.width = `${size}ch`;
+}
